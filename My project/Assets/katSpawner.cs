5e192b46e1f799c2dab57d649759db22eb768d18
Zephoenixen <stormriberengelund@gmail.com>
2024-01-25 23:42:38 +0100

    Choosing Arrays Converted to Lists

--- My project/Assets/katSpawner.cs
+++ My project/Assets/katSpawner.cs
@@ -11,17 +11,30 @@ public class KatSpawner : MonoBehaviour
     public bool gameStarted = true;
     public int[] spwPos;
     public GameObject[] Kats;
-    public List<GameObject> SpawnsClear;
+    public List<int> SpawnsClear;
+    public List <int> KatsClear;
   
     private void Start()
     {
-        
+        SpawnsClear.Clear();
+        KatsClear.Clear();
+        for (int i = 0; i < spwPos.Length; i++) 
+        {
+            print("Spawn Count " + i);
+            SpawnsClear.Add(i);
+
+        }
+        for (int i = 0; i < Kats.Length; i++)
+        {
+            print("Kat Count " + i);
+            KatsClear.Add(i);
+        }
     }
 
     void Pick()
     {
-        int randomKat = Random.Range(0, Kats.Length);
-        int randomSpw = Random.Range(0, spwPos.Length);
+        int randomKat = Random.Range(0, KatsClear.Count);
+        int randomSpw = Random.Range(0, SpawnsClear.Count);
         GameObject clone = Instantiate(Kats[randomKat], new Vector3(spwPos[randomSpw],katSpwnHeight,0), Quaternion.identity);
     }
 
