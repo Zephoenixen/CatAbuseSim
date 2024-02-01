755c1a15938ef2e475954f858f80cf81114b5a78
digicrab <76408791+digicrab@users.noreply.github.com>
2024-02-01 17:58:03 +0100

    Fernando is noisy

--- katti slappi/Assets/Fernandobonk.cs
+++ katti slappi/Assets/Fernandobonk.cs
@@ -6,14 +6,27 @@ using UnityEngine.SceneManagement;
 
 public class Fernandobonk : MonoBehaviour
 {
-
+    // Sprites der bliver skiftet imellem
     public SpriteRenderer prebonk;
     public SpriteRenderer postbonk;
     public SpriteRenderer nohammer;
     public SpriteRenderer amgry;
+    public SpriteRenderer Gameover;
+
+    //Objecter der bliver rykket rundt på
     public GameObject Wham;
     public GameObject Hands;
     public GameObject Camera;
+
+    //Lyd effekter
+    public AudioSource growlsfx;
+    public AudioSource bonksfx;
+    public AudioSource snapsfx;
+
+    //Baggrundsfarve efter gameover kommer
+    public Color gameovercolor;
+
+    //FadeDone modtager et signal fra et andet script og starter så Bonking Inumeratoren
     void FadeDone()
     {
 
@@ -22,33 +35,74 @@ public class Fernandobonk : MonoBehaviour
     }
     IEnumerator Bonking()
     {
+        //Dette trin for Wham visual effekten til at være på skærmen, ændrer spriten til at være i bonket tilstand, og afspiller Bonk Sfx
         Wham.transform.position = new Vector3(-0.82f, 4.44f, 0);
         BroadcastMessage("Sound", "Bonk", SendMessageOptions.DontRequireReceiver);
         prebonk.sprite = postbonk.sprite;
 
+        //Dette trin venter og dernæst fjerner Wham visual effekten
         yield return new WaitForSeconds(0.5f);
         Wham.transform.position = new Vector3(-0.82f, 4.44f, -60);
 
+        //Dette trin venter lidt længere for at ændre spriten for at fjerne hammeren, og starter Growl Sfx sådan den spiller i tide til at det giver mening
         yield return new WaitForSecondsRealtime(1.5f);
         prebonk.sprite = nohammer.sprite;
-
-        yield return new WaitForSeconds(1.5f);
-        prebonk.sprite = amgry.sprite;
         BroadcastMessage("Sound", "Growl");
 
+        //Dette trin venter ligeså langt som det sidste for at ændre sprite til vred tilstand, og sende signal til hænderne at de skal bevæge sig
+        yield return new WaitForSeconds(1.5f);
+        prebonk.sprite = amgry.sprite;
+        
         BroadcastMessage("Handtime",SendMessageOptions.DontRequireReceiver);
 
+        //Dette trin vænter indtil hænderne stopper og rotere cameraet sådan det ligner din nak bliver brækket og spiller Snap sfx
         yield return new WaitForSeconds(3f);
         Camera.transform.Rotate(0, 60, 0);
         BroadcastMessage("Sound", "Snap", SendMessageOptions.DontRequireReceiver);
 
+        //Dette trin rykker cameraet lidt længere for at gøre man ikke kan se nogle sprites og ændrer baggrunds farven til at matche resten af spillet og viser dernæst en game over skærm
         yield return new WaitForSeconds(0.5f);
         Camera.transform.Rotate(0, 40, 0);
+        Camera.GetComponent<Camera>().backgroundColor = gameovercolor;
+        Gameover.color = new Color(1,1,1,1);
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            SceneManager.LoadScene("Menu Screen");
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            SceneManager.LoadScene("Menu Screen");
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            SceneManager.LoadScene("Menu Screen");
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            SceneManager.LoadScene("Menu Screen");
+        }
 
-        yield return new WaitForSeconds(0.6f);
-        SceneManager.LoadScene("Menu Screen");
 
         yield break;
+    }
+    void Sound(string Soundtype)
+    {
+        if (Soundtype == "Growl")
+        {
+            growlsfx.Play();
+        }
+        else if (Soundtype == "Bonk")
+        {
+            bonksfx.Play();
+        }
+        else if (Soundtype == "Snap")
+        {
+            snapsfx.Play();
+        }
+        else
+        {
+            Debug.Log("Sound broke");
+        }
     }
 }
 
